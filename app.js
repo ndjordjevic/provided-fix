280dd6923ed2cd430adf03d69ff89f120ddc26dc
ndjordjevic <ndjordjevic@gmail.com>
2015-04-13 17:59:36 +0200

    Added code to replace file content

--- app.js
+++ app.js
@@ -3,15 +3,26 @@
  */
 
 var walk = require('walk');
+var fs = require('fs');
 var files = [];
 
-// Walker options
+// Walker options TODO: populate 'filters'
 var walker = walk.walk('testFolder', {followLinks: false, filters: ["Temp", "_Temp"]});
 
 walker.on('file', function (root, stat, next) {
     // Add this file to the list of files
     if (stat.name === 'pom.xml') {
         files.push(root + '/' + stat.name);
+        fs.readFile(root + '/' + stat.name, 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            var result = data.replace(/provided/g, 'compile');
+
+            fs.writeFile(root + '/' + stat.name, result, 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+        });
     }
     next();
 });
