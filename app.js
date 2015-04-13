948fab448e8547bfb3f8cc62d3c96acadef73150
ndjordjevic <ndjordjevic@gmail.com>
2015-04-13 16:49:38 +0200

    Added test folder structure

--- app.js
+++ app.js
@@ -2,22 +2,28 @@
  * Created by ndjordjevic on 4/12/15.
  */
 
-var walk    = require('walk');
-var files   = [];
+var walk = require('walk');
+var files = [];
 
 // Walker options
-var walker  = walk.walk('.', { followLinks: false });
+var walker = walk.walk('testFolder', {followLinks: false, filters: ["Temp", "_Temp"]});
 
-walker.on('file', function(root, stat, next) {
+walker.on('file', function (root, stat, next) {
     // Add this file to the list of files
-    files.push(root + '/' + stat.name);
+    if (stat.name === 'pom.xml') {
+        files.push(root + '/' + stat.name);
+    }
     next();
 });
 
-walker.on('names', function (root, nodeNamesArray) {
-    console.log(nodeNamesArray);
-});
+//walker.on('names', function (root, nodeNamesArray) {
+//    console.log(nodeNamesArray);
+//});
 
-walker.on('end', function() {
+walker.on('end', function () {
     console.log(files);
 });
+
+//walker.on('files', function(root, stats, next) {
+//    console.log(root);
+//});
